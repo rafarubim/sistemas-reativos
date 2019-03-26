e49eb46a3def332c034eeb92df209715fd8108c2
rafarubim <rafael.rubim.cabral@gmail.com>
2019-03-26 18:57:59 -0300

    Add start of miniProject1

---
+++ miniProjeto1/event_driven/app.ino
@@ -0,0 +1,92 @@
+#include "app.h"
+#include "pindefs.h"
+#include "event_driven.h"
+
+#define DEBOUNCE_DURATION_MS 1000
+
+#define DEBOUNCE_1_INDEX 0
+#define DEBOUNCE_2_INDEX 1
+#define DEBOUNCE_3_INDEX 2
+
+#define TIMER_DEBOUNCE_1 1
+#define TIMER_DEBOUNCE_2 2
+#define TIMER_DEBOUNCE_3 3
+
+static int isDebounceBlocked[3] = {0, 0, 0};
+static int timerByDebounceInx[3] = {TIMER_DEBOUNCE_1, TIMER_DEBOUNCE_2, TIMER_DEBOUNCE_3};
+
+static int val = 0;
+
+static void debouncedButtonChanged();
+
+static void buttonChanged(int pin, int value);
+
+void appinit(void) {
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(LED4, OUTPUT);
+  digitalWrite(LED1, HIGH);
+  digitalWrite(LED2, HIGH);
+  digitalWrite(LED3, HIGH);
+  digitalWrite(LED4, HIGH);
+  button_listen(KEY1);
+  button_listen(KEY2);
+}
+
+void button_changed(int p, int v) {
+  switch(p) {
+    case KEY1:
+      debouncedButtonChanged(DEBOUNCE_1_INDEX, p, v);
+      break;
+    case KEY2:
+      debouncedButtonChanged(DEBOUNCE_2_INDEX, p, v);
+      break;
+    case KEY3:
+      debouncedButtonChanged(DEBOUNCE_3_INDEX, p, v);
+      break;
+  }
+}
+
+void timer_expired(int timer) {
+  switch(timer) {
+    case TIMER_DEBOUNCE_1:
+      isDebounceBlocked[DEBOUNCE_1_INDEX] = 0;
+      break;
+    case TIMER_DEBOUNCE_2:
+      isDebounceBlocked[DEBOUNCE_2_INDEX] = 0;
+      break;
+    case TIMER_DEBOUNCE_3:
+      isDebounceBlocked[DEBOUNCE_3_INDEX] = 0;
+      break;
+  }
+}
+
+static void buttonChanged(int pin, int value) {
+  if(!value) {
+    val += 1;
+  }
+  switch(val) {
+     case 4:
+      digitalWrite(LED4, LOW);
+     case 3:
+      digitalWrite(LED3, LOW);
+     case 2:
+      digitalWrite(LED2, LOW);
+     case 1:
+      digitalWrite(LED1, LOW);
+  }
+}
+
+void debouncedButtonChanged(int debounceInx, int pin, int value) {
+
+  int debounceTimer = timerByDebounceInx[debounceInx];
+  
+  timer_set(debounceTimer, DEBOUNCE_DURATION_MS);
+  
+  if (!isDebounceBlocked[debounceInx]) {
+    isDebounceBlocked[debounceInx] = 1;
+    
+    buttonChanged(pin, value);
+  }
+}
